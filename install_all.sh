8a32844f59062437dfdad59cb148fd0fc823ca08
Malte Hoffmann <malte.hoffmann@siemensgamesa.com>
2022-10-26 16:48:56 +0000

    fix lcl setup

--- install_all.sh
+++ install_all.sh
@@ -10,11 +10,21 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv --version
 
+# Install python version
 pyenv install -v 3.9.15
 pyenv global 3.9.15
 
+# Install venv
+python -m venv venv
+# https://pythonspeed.com/articles/activate-virtualenv-dockerfile/
 export VIRTUAL_ENV=./venv
 export PATH="$VIRTUAL_ENV/bin:$PATH"
-python -m venv venv
-source ./venv/bin/activate
+# https://unix.stackexchange.com/questions/21598/how-do-i-set-a-user-environment-variable-permanently-not-session
+echo "export VIRTUAL_ENV=./venv" >> ${HOME}/.bash_profile
+echo 'export PATH="$VIRTUAL_ENV/bin:$PATH"' >> ${HOME}/.bash_profile
+echo 'export LINK=https://sensorndf.azurewebsites.net/sensor-data' >> ${HOME}/.bash_profile
+# Local Testing: echo 'export LINK=http://127.0.0.1:5000/sensor-data' >> ${HOME}/.bash_profile
+# https://askubuntu.com/questions/121073/why-bash-profile-is-not-getting-sourced-when-opening-a-terminal
+echo ". ~/.bash_profile" >> ${HOME}/.bashrc
 pip install --no-cache-dir -r requirements.txt
+
