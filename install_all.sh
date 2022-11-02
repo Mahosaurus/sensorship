077ddd1990a6ac63dd57bbccc2dcff97fa2b5d72
Mahosaurus <Mahosaurus@users.noreply.github.com>
2022-11-02 16:34:48 +0100

    Fixing linux dependencied

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,8 @@
 # Prerequisites for pyenv
-apt-get install -yy make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+sudo apt update
+sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # Pyenv installation according to https://realpython.com/intro-to-pyenv/
 curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
