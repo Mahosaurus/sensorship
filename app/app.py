2aeaaab7a3800c7578c1272c8e9e43773e412780
Malte Hoffmann <malte.hoffmann@siemensgamesa.com>
2022-11-07 08:20:29 +0000

    merge master

--- app/app.py
+++ app/app.py
@@ -27,14 +27,14 @@ def save_to_file(out_str):
     with open("outcome_local.txt", "a", encoding="utf-8") as filehandle:
         filehandle.write(out_str)
 
-# def send_to_app(out_str):
-#     """ Try to send metrics to App """
-#     try:
-#         dict_to_send = {"data": out_str}
-#         res = requests.put(os.environ['LINK'], json=dict_to_send, verify=False)
-#         print(res, res.text)
-#     except Exception as exc:
-#         print(f"Error in sending metrics to App: {exc}")
+def send_to_app(out_str):
+    """ Try to send metrics to App """
+    try:
+        dict_to_send = {"data": out_str}
+        res = requests.put(os.environ['LINK'], json=dict_to_send, verify=False)
+        print(res, res.text)
+    except Exception as exc:
+        print(f"Error in sending metrics to App: {exc}")
 
 def main():
     """ Function that gets called by scheduler. """
@@ -42,21 +42,13 @@ def main():
     current_ts = time.time()
     out_str = compile_data_point(current_ts, temperature, humidity)
     save_to_file(out_str)
-<<<<<<< Updated upstream
     if not cfg.TEST_MODE:
         send_to_app(out_str)
-=======
-    #send_to_app(out_str)
->>>>>>> Stashed changes
 
 # Call it once to test it works, even with long scheduler
 main()
 sched = BackgroundScheduler(daemon=True)
-<<<<<<< Updated upstream
 sched.add_job(main, 'interval', seconds=cfg.PERIOD)
-=======
-sched.add_job(main, 'interval', seconds=1)
->>>>>>> Stashed changes
 sched.start()
 
 app = Flask(__name__)
