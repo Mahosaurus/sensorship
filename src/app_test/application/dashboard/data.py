7286f325307065325c4012e99c0c025037d6a47a
Mahosaurus <metaphigmamentality@web.de>
2022-12-27 07:34:18 +0100

    start to impl dash

---
+++ src/app_test/application/dashboard/data.py
@@ -0,0 +1,18 @@
+
+"""Prepare data for Plotly Dash."""
+import numpy as np
+import pandas as pd
+import os
+
+from src.config import get_repo_root
+
+def create_dataframe():
+    """Create Pandas DataFrame from local CSV."""
+    path = os.path.join(get_repo_root(), "app_test", "data", "311-calls.csv")
+    df = pd.read_csv(path, parse_dates=["created"])
+    df["created"] = df["created"].dt.date
+    df.drop(columns=["incident_zip"], inplace=True)
+    num_complaints = df["complaint_type"].value_counts()
+    to_remove = num_complaints[num_complaints <= 30].index
+    df.replace(to_remove, np.nan, inplace=True)
+    return df
---
+++ src/app_test/application/routes.py
@@ -0,0 +1,74 @@
+
+from src.utils.io_interaction import read_as_str_from_disk, read_as_pandas_from_disk, write_pandas_data_to_disk, pandas_to_str
+from src.config import APP_TEST_DATA_PATH
+
+from flask import Response, render_template
+from flask import current_app as app
+
+@app.route("/")
+def home():
+    """Landing page."""
+    return render_template(
+        "index.jinja2",
+        title="Plotly Dash Flask Tutorial",
+        description="Embed Plotly Dash into your Flask applications.",
+        template="home-template",
+        body="This is a homepage served with Flask.",
+    )
+
+
+@app.route("/text-data")
+def text_data():
+    data = read_as_str_from_disk(APP_TEST_DATA_PATH)
+    return data.split("\n")
+
+from src.utils.aggregator import aggregate
+
+@app.route("/aggregate-data")
+def aggregate_data():
+    data = read_as_pandas_from_disk(APP_TEST_DATA_PATH)
+    aggregated_data = aggregate(data)
+    write_pandas_data_to_disk(aggregated_data, APP_TEST_DATA_PATH)
+    return "Success"
+
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from src.utils.graphics import PlotSensor
+from src.utils.predictor import Predictor
+
+@app.route("/show-data")
+def plot_png():
+    # Read existing data
+    data = read_as_str_from_disk(APP_TEST_DATA_PATH)    
+    # Add predicted data
+    pred_data = read_as_pandas_from_disk(APP_TEST_DATA_PATH)
+    predictor = Predictor(pred_data)
+    result = predictor.make_lstm_prediction()
+    pred_data = pandas_to_str(result)
+    # Concat
+    data = data + pred_data
+    # Plot
+    plotter = PlotSensor(data)    
+    fig = plotter.create_figure(len(pred_data))
+    output = io.BytesIO()
+    FigureCanvasAgg(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+import os
+import time
+from src.config import get_repo_root
+
+def save_to_file(out_str):
+    """ Write metrics to file """
+    with open(os.path.join(get_repo_root(), "outcome_local.txt"), "a", encoding="utf-8") as filehandle:
+        filehandle.write(out_str)
+
+from src.utils.helpers import compile_data_point
+from src.utils.tempsensor import get_mock_data
+
+def main():
+    """ Function that gets called by scheduler. """
+    temperature, humidity = get_mock_data()
+    current_ts = time.time()
+    out_str = compile_data_point(current_ts, temperature, humidity)
+    save_to_file(out_str)    
