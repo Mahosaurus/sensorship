be25f0b7bdbf9929bc95d5cccee931ba5c98367d
maho <maho>
2023-01-06 06:50:17 +0000

    update build

--- .deployment/install_dev_docker.sh
+++ .deployment/install_dev_docker.sh
@@ -3,3 +3,8 @@ pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r requirements.txt
 pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/api/requirements.txt
 pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/app_raspi/requirements.txt
 pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/predictor/requirements.txt
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash --insecure
+str="if [ -f ~/.git-completion.bash ] ; then . ~/.git-completion.bash ; fi"
+echo $str >> ~/.bashrc
+echo "Done"
