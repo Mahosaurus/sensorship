79da4e2258a18643b0886969d438125d50dffe30
Mahosaurus <Mahosaurus@users.noreply.github.com>
2023-02-07 16:45:17 +0100

    small fixes (#39)
    Co-authored-by: maho <maho>

--- .deployment/install_dev_docker.sh
+++ .deployment/install_dev_docker.sh
@@ -1,8 +1,8 @@
-python src/setup.py develop
+sudo python src/setup.py develop
 pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r requirements.txt
-pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/api/requirements.txt
-pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/app_raspi/requirements.txt
-pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r src/predictor/requirements.txt
+pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r api/requirements.txt
+pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r app_raspi/requirements.txt
+pip3 --cert /etc/ssl/certs/ca-certificates.crt install -r ml/requirements.txt
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash --insecure
 str="if [ -f ~/.git-completion.bash ] ; then . ~/.git-completion.bash ; fi"
